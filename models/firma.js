a7b5cb2a23016c8df0afbb2739e10f4a2b20999d
tope96 <pietrzaktomek@hotmail.com>
2019-03-28 22:36:44 +0100

    szablon logowania - dziala

--- models/firma.js
+++ models/firma.js
@@ -30,6 +30,6 @@ module.exports = function(sequelize, DataTypes) {
     }
   }, {
     tableName: 'firma',
-    timestamps: false
+    timestamps:false
   });
 };
--- models/uzytkownik.js
+++ models/uzytkownik.js
@@ -33,6 +33,7 @@ module.exports = function(sequelize, DataTypes) {
       allowNull: false
     }
   }, {
-    tableName: 'uzytkownik'
+    tableName: 'uzytkownik',
+    timestamps: false
   });
 };
