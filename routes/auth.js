96a7f68c10a29336ad7db411121efbff7729c845
tope96 <pietrzaktomek@hotmail.com>
2019-03-28 12:57:08 +0100

    poprawki - logowanie

--- routes/auth.js
+++ routes/auth.js
@@ -23,7 +23,7 @@ module.exports = function (app, passport) {
     }
 
     app.post('/signup', passport.authenticate('local-signup', {
-        successRedirect: '/UserChoose',
+        successRedirect: '/home',
         failureRedirect: '/notConfirmedUSer'
     }
     ));
