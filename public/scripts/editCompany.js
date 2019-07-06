c4c823c95615fa9db819fb60607dcb3a51cc17bd
tope96 <pietrzaktomek@hotmail.com>
2019-07-06 22:37:07 +0200

    trzecie poprawki wygladu

---
+++ public/scripts/editCompany.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+    //makes submit button disable when inputs are empty
+    $(document).ready(function () {
+        $('.input').keyup(function () {
+
+            var empty = false;
+            $('.input').each(function () {
+                if ($(this).val().length == 0 || $('.selectWorker').val() == 'Brak pracowników bez kont' || $('.selectWorker').val() == 'Wybierz pracownika') {
+                    empty = true;
+                }
+            });
+
+            if (empty) {
+                $('.continue').attr('disabled', 'disabled');
+            } else {
+                $('.continue').attr('disabled', false);
+            }
+        });
+
+        $('.selectWorker').click(function () {
+            var empty = false;
+            $('.input').each(function () {
+                if ($(this).val().length == 0 || $('.selectWorker').val() == 'Brak pracowników bez kont' || $('.selectWorker').val() == 'Wybierz pracownika') {
+                    empty = true;
+                }
+            });
+
+            if (empty) {
+                $('.continue').attr('disabled', 'disabled');
+            } else {
+                $('.continue').attr('disabled', false);
+            }
+        });
+    });
+
+});
